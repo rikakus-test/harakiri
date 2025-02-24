15c751ce3168568dc62a9e95fb1b972fbbc538bf
rikakus <rikakus082@gmail.com>
2025-02-24 15:54:07 +0700

    next commit

--- index.js
+++ index.js
@@ -30,24 +30,20 @@ const PORT = 3020;
 const DATA_FILE = path.join(__dirname, "data.json");
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Fungsi untuk membaca data dari file JSON
 const readData = () => {
   const rawData = fs.readFileSync(DATA_FILE);
   return JSON.parse(rawData);
 };
 
-// Fungsi untuk menulis data ke file JSON
 const writeData = (data) => {
   fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
 };
 
-// Endpoint: Mendapatkan semua data
 app.get("/items", (req, res) => {
   const data = readData();
   res.json(data);
 });
 
-// Endpoint: Menambah item baru
 app.post("/items", (req, res) => {
   const data = readData();
   const newItem = {
@@ -69,7 +65,6 @@ app.put("/items/:id", (req, res) => {
     return res.status(404).json({ message: "Item not found" });
   }
 
-  // Update item yang ditemukan
   data[index] = {
     ...data[index], // Pertahankan data lama
     name: req.body.name || data[index].name, // Update jika ada, jika tidak tetap sama
@@ -80,7 +75,6 @@ app.put("/items/:id", (req, res) => {
   res.json(data[index]); // Kirim data yang sudah diperbarui
 });
 
-// Endpoint: Menghapus item berdasarkan ID
 app.delete("/items/:id", (req, res) => {
   let data = readData();
   const id = parseInt(req.params.id);
@@ -89,7 +83,6 @@ app.delete("/items/:id", (req, res) => {
   res.json({ message: "Item deleted", id });
 });
 
-// Jalankan server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
