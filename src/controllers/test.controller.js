f5a49977992403747d305721994efcc22ab2e285
rikakus <ianpangestu082@gmail.com>
2025-06-15 10:51:36 +0700

    test mm

--- src/controllers/test.controller.js
+++ src/controllers/test.controller.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require("uuid");
 const moment = require("moment");
 const date = require("../helpers/date");
 const itemsModel = require("../models/test.model");
-const { io } = require("../helpers/sockets");
+const { io } = require("../helpers/socket");
 
 module.exports = {
   items: async (req, res) => {
